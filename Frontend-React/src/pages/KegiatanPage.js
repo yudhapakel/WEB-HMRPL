b755869e7dad539b81e9bc4544a52cfd229aaa1e
Naufal(ganteng) <ahmadnaufalalghifari6@gmail.com>
2025-07-20 19:58:57 +0700

    menambahkan halaman kegiatan pada fe

---
+++ Frontend-React/src/pages/KegiatanPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Kalender from '../components/Kalender/Kalender'; 
+import GaleriKegiatan from '../components/Kegiatan/GaleriKegiatan'; 
+
+const KegiatanPage = () => {
+  return (
+    <>
+      <Kalender />
+      <GaleriKegiatan />
+    </>
+  );
+};
+
+export default KegiatanPage;
