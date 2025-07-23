0c30199087b19c8c673b8884b5536a6bcbecb449
yudhapakel <yudahp23@gmail.com>
2025-07-23 21:32:50 +0700

    fix: endpoint API for login and logout

--- Backend-Laravel/routes/api.php
+++ Backend-Laravel/routes/api.php
@@ -7,3 +7,16 @@ use App\Http\Controllers\AuthController;
 // Login + Logout routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+    return response()->json($request->user());
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/dasboard', [DasboardController::class, 'index']);
+});
+
+// Route::middleware('auth:sanctum')->post('/logout-debug', function (Request $request) {
+//     return response()->json([
+//         'user' => $request->user(),
+//         'token' => $request->bearerToken()
+//     ]);
+// });
