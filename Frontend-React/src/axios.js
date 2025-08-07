ef98b99cee521dd98775c7f8ecea4f1504790d69
yudhapakel <yudahp23@gmail.com>
2025-08-07 14:40:02 +0700

    wip: conecting be and fe test

---
+++ Frontend-React/src/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000/api',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${token}'
+    },
+    withCredentials: true,
+});
+
+export default api;
